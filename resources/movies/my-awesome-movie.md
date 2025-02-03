9df8a40675b46ac924611d666e23cf98d84c5353
Felipe Jovino <Felipejovinogamerplay@gmail.com>
2025-02-03 15:34:16 -0300

    Suporte para markdown

---
+++ resources/movies/my-awesome-movie.md
@@ -0,0 +1,3 @@
+# My Awesome Movie
+
+Lorem ipsum dolor sit amet, culpa ex ullamco ut officia sit sint laboris doloreLorem cupidatat labore pariatur nulla tempor ullamco in ullamco. Mollit dolor ve lit voluptate. Minim culpa Lorem dolor irure ullamco est labore in mollit ut nos trud voluptate. Laborum nulla aute minim incididunt eiusmod aliqua quis reprehen derit adipisicing qui irure sit fugiat culpa. Cupidatat non eiusmod elit in eu e x ullamco consectetur nostrud cupidatat ea tempor veniam et et voluptate dolore Lorem consequat.
--- start/routes.ts
+++ start/routes.ts
@@ -2,16 +2,22 @@ import { Exception } from '@adonisjs/core/exceptions'
 import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
 import fs from 'node:fs/promises'
+import { MarkdownFile } from '@dimerapp/markdown'
+import { toHtml } from '@dimerapp/markdown/utils'
 
 router.on('/').render('pages/home').as('home')
 
 router
   .get('/movies/:slug', async (ctx) => {
-    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
-    let movie: any
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.md`)
 
     try {
-      movie = await fs.readFile(url, 'utf8')
+      const file = await fs.readFile(url, 'utf8')
+      const md = new MarkdownFile(file)
+
+      await md.process()
+
+      const movie = toHtml(md).contents
       ctx.view.share({ movie })
     } catch (error) {
       throw new Exception(`Não conseguir encontrar um filme chamado ${ctx.params.slug}`, {
