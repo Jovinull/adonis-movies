31417469779b654d4e2142e846b961d50293c8f0
Felipe Jovino <Felipejovinogamerplay@gmail.com>
2025-02-03 11:04:20 -0300

    Start

---
+++ start/routes.ts
@@ -0,0 +1,26 @@
+import { Exception } from '@adonisjs/core/exceptions'
+import app from '@adonisjs/core/services/app'
+import router from '@adonisjs/core/services/router'
+import fs from 'node:fs/promises'
+
+router.on('/').render('pages/home').as('home')
+
+router
+  .get('/movies/:slug', async (ctx) => {
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    let movie: any
+
+    try {
+      movie = await fs.readFile(url, 'utf8')
+      ctx.view.share({ movie })
+    } catch (error) {
+      throw new Exception(`Não conseguir encontrar um filme chamado ${ctx.params.slug}`, {
+        code: 'E_NOT_FOUND',
+        status: 404,
+      })
+    }
+
+    return ctx.view.render('pages/movies/show')
+  })
+  .as('movies.show')
+  .where('slug', router.matchers.slug())
